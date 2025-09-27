0f4628ea801299fcf50103ece120e9b532ef7cc3
sanekha <sanekhakkclt@gmail.com>
2025-09-27 15:02:38 +0530

    deploying erros fixed

--- Katlo/settings.py
+++ Katlo/settings.py
@@ -1,8 +1,11 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY', 'a-default-secret-key-for-local-dev')
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-development')
+
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
@@ -52,11 +55,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Katlo.wsgi.application'
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
